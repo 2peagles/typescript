ab991778437d0a73c4f38e36d4bdfad8478fddd9
Precious <precioud.d.bey@gmail.com>
2022-07-21 17:26:25 -0400

    cart

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,24 @@
+import { Routes, Route} from 'react-router-dom';
+import { Container } from 'react-bootstrap';
+import { Home } from './pages/Home';
+import { Store } from './pages/store';
+import { About } from './pages/About';
+import {Navbar} from './components/Navbar';
 import './App.css';
-import { Container } from './components/props/Container';
-import { Greet } from './components/props/Greet';
-import PeronsList from './components/props/PeronsList';
-import { Person } from './components/props/Person';
-import { Status } from './components/props/status';
-import { Counter } from './components/State/Counter';
+import { ShoppingCartProvider } from './context/ShoppingCartContext';
 
 function App() {
-
-  const personName={
-    first:'Niall',
-    last:'Horan',
-  }
-
-  const nameList = [
-    {
-      first: 'Harry',
-      last: 'Styles',
-    },
-    {
-      first: 'Louis',
-      last: 'Tomo',
-    },
-  ]
-
   return (
-    <div className="App">
-      <Greet  name='Precious' messageCount={20} isLoggedIn={false}/>
-      <Person name={personName} />
-      <PeronsList names={nameList}/>
-      <Status status='loading'/>
-      <Container styles={{ border: '1px solid black', padding: '1rem', margin: '10px 50px' }} />
-      <Counter/>
-    </div>
+    <ShoppingCartProvider>
+    <Navbar />
+    <Container  className="mb-4">
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/store' element={<Store />} />
+        <Route path='/about' element={<About />} />
+      </Routes>
+    </Container>
+    </ShoppingCartProvider>
   );
 }
 
--- src/components/props/Person.tsx
+++
@@ -1,7 +0,0 @@
-import { PersonProps } from './Person.types';
-
-export const Person = (props: PersonProps) => {
-  return (
-    <div><h1>{props.name.first} {props.name.last}</h1></div>
-  )
-}

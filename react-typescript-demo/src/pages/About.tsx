afb038bc5901966e415801152c9181296c49a096
Precious <precioud.d.bey@gmail.com>
2022-07-29 19:21:10 -0400

    page structures

--- react-typescript-demo/src/pages/About.tsx
+++ react-typescript-demo/src/pages/About.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
+import React from 'react';
+import {Container, Row, Col} from 'react-bootstrap';
 
 export const About = () => {
   return (
-    <div>About</div>
+    <Container fluid >
+      <Row>
+        <Col className='d-flex align-items-center justify-content-center'> About Us </Col>
+      </Row>
+      <Row className='d-flex align-items-center justify-content-center'>
+        <Col className='d-flex align-items-center justify-content-center '> Img Here </Col>
+        <Col className='d-flex align-items-center justify-content-center'> Mission Statement Here  </Col>
+      </Row>
+      <Row>
+        <Container>
+          <Row>
+          <Col> icon w/h text above ()</Col>
+            <Col> icon w/h text above ()</Col>
+              <Col> icon w/h text above ()</Col>
+              <Col> icon w/h text above ()</Col>
+            <Col> icon w/h text above ()</Col>
+          <Col> icon w/h text above ()</Col>
+          </Row>
+        </Container>
+      </Row>
+      <Row>
+        <Row> <Col className='d-flex align-items-center justify-content-center'> title here </Col></Row>
+        <Row>
+          <Col className='d-flex align-items-center justify-content-center '> something here</Col>
+          <Col className='d-flex align-items-center justify-content-center '>
+             something here
+          </Col>
+         </Row>
+      </Row>
+      <Row>
+        <Col className='d-flex align-items-center justify-content-center text-align-center '> Eco statement here </Col>
+      </Row>
+    </Container>
   )
 }
--- react-typescript-demo/src/pages/Home.tsx
+++ react-typescript-demo/src/pages/Home.tsx
@@ -1,7 +1,38 @@
-import React from 'react'
+import React from 'react';
+import {Container, Col, Row} from 'react-bootstrap';
 
 export const Home = () => {
   return (
-    <div>Home</div>
+    <Container fluid >
+      <Row>
+        <Col className='d-flex align-items-center justify-content-center'> Home </Col>
+      </Row>
+      <Row>
+        <Container>
+          <Row>
+            <Col> icon w/h text above ()</Col>
+            <Col> icon w/h text above ()</Col>
+            <Col> icon w/h text above ()</Col>
+            <Col> icon w/h text above ()</Col>
+          </Row>
+        </Container>
+      </Row>
+      <Row>
+        <Row>
+          <Col className='d-flex align-items-center justify-content-center '> something here</Col>
+          <Col className='d-flex align-items-center justify-content-center '>
+            something here
+          </Col>
+        </Row>
+      </Row>
+      <Row>
+        <Row>
+          <Col className='d-flex align-items-center justify-content-center '> something here</Col>
+          <Col className='d-flex align-items-center justify-content-center '>
+            something here
+          </Col>
+        </Row>
+      </Row>
+    </Container>
   )
 }
